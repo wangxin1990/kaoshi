3a2b1bc1fe4244e84a70a380bede7c945b4fbac5
wangxin1990 <825249167@qq.com>
2016-01-17 20:51:20 +0800

    update

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php 
-$websit_name="窗内网（http://www.itzcn.com）----网上考试";//站点名称
+$websit_name="网上考试";//站点名称
 $websit_root="http://localhost/kaoshi/";//站点根目录
 $cnumber=50;//单选题条数
 $jnumber=50;//判断题条数
